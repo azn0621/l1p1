62f042e92acce79211558576e4871b1e704ead99
Ayaan <144817365+azn0621@users.noreply.github.com>
2026-08-29 12:41:03 +0100

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,13 @@
+name = input("Enter your real name, Agent: ")
+gadget = input("Enter your favourite gadget: ")
+
+agent_number = 7
+speed_rating = 9.5
+mission_count = 12
+
+print("name:", name, " -> type:", type(name))
+print("gadget:", gadget, " -> type:", type(gadget))
+
+print("Agent Number:", agent_number, " -> type:", type(agent_number))
+print("Speed rating:", speed_rating, " -> type:", type(speed_rating))
+print("Mission Count :", mission_count, " -> type:", type(mission_count))
