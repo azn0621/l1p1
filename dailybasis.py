6d58634347c04a670767cd6f34400f1801b9c27f
Ayaan <144817365+azn0621@users.noreply.github.com>
2026-09-25 20:34:41 +0100

    Add files via upload

---
+++ dailybasis.py
@@ -0,0 +1,9 @@
+feeling =  int(input( "Enter how are you feeling"))
+if feeling  < 35
+    activity = "indoor board games"
+    print("It is an okay day") 
+    print("Do,activity")
+else:
+    activity = outdoor walking
+    print("It is a great day")
+    print("Do,activity")      
